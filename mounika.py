67b66f4aee4cb24147c5169a78174de677d6bd8f
Mounika <pinkymounika414@gmail.com>
2023-05-18 12:11:22 +0530

    arthimetic operations

--- mounika.py
+++ mounika.py
@@ -1,15 +1,10 @@
-def is_palindrome(string):
-    #convert to lowercase and remove non-alphabetic character
-    string = "'".join(c.lower() for c in string if c.isalpha())
-    #compare to its reverse
-    reversed_string = "".join(reversed(string))
-    return string == reversed_string
-#get user input for the string
-user_input = input("Enter a string")
-#check if the string is a palindrome
-if is_palindrome(user_input):
-    print(f"{user_input} is a palindrome.")
-else:
-    print(f"{user_input} is not a palindrome.")    
-
+if __name__ == '__main__'
+#arithmetic operations
+a = int(input())
+b = int(input())
+c = a + b
+d = a - b
+e = a * b
+print(c,d,e,sep = "/n")
+print(f"{a//b}\n{a/b}")
 
