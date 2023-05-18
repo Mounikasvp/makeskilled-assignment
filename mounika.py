e01a6b4f4a1fc9245092eac3acf425b777105d46
Mounika <pinkymounika414@gmail.com>
2023-05-18 09:19:34 +0530

    changed from hello world to palindrome

--- mounika.py
+++ mounika.py
@@ -1,2 +1,15 @@
-n = input("Hi There! ")
-print(n,"Mounika")
+def is_palindrome(string):
+    #convert to lowercase and remove non-alphabetic character
+    string = "'".join(c.lower() for c in string if c.isalpha())
+    #compare to its reverse
+    reversed_string = "".join(reversed(string))
+    return string == reversed_string
+#get user input for the string
+user_input = input("Enter a string")
+#check if the string is a palindrome
+if is_palindrome(user_input):
+    print(f"{user_input} is a palindrome.")
+else:
+    print(f"{user_input} is not a palindrome.")    
+
+
